7a29da2a952383294285b8fd129d9ae2d2ad83d5
SpectraPhantom <kbarszczyk@protonmail.com>
2021-05-06 13:57:57 +0200

    Added external api url , UriComponentsBuilder

--- src/main/java/com/restapp/springrestclient/controllers/UserController.java
+++ src/main/java/com/restapp/springrestclient/controllers/UserController.java
@@ -5,17 +5,15 @@ import com.restapp.springrestclient.services.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ServerWebExchange;
 
 @Controller
 @Slf4j
 public class UserController {
 
-    private ApiService apiService;
+    private final ApiService apiService;
 
     public UserController(ApiService apiService) {
         this.apiService = apiService;
@@ -27,13 +25,9 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String formPost(Model model, @ModelAttribute("limit") int limit) {
-//        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
-//
-//        Integer limit = Integer.valueOf(map.get("limit").get(0));
+    public String formPost(Model model, @ModelAttribute("limit") Integer limit) {
 
         log.debug("Received Limit value: " + limit);
-
         if (limit == 0) {
             log.debug("Setting limit to default of 10");
             limit = 10;
--- src/main/java/com/restapp/springrestclient/services/ApiServiceImpl.java
+++ src/main/java/com/restapp/springrestclient/services/ApiServiceImpl.java
@@ -1,23 +1,36 @@
 package com.restapp.springrestclient.services;
 
 import com.restapp.springrestclient.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class ApiServiceImpl implements ApiService {
 
     private final RestTemplate restTemplate;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    private final String api_url;
+
+    public ApiServiceImpl(RestTemplate restTemplate,@Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        List<User> userData=restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit="+limit,List.class);
+        log.debug(api_url);
+        UriComponentsBuilder uriComponentsBuilder=UriComponentsBuilder.
+                fromUriString(api_url).
+                queryParam("_limit",limit);
+
+        List<User> userData=restTemplate.getForObject(uriComponentsBuilder.toUriString(),List.class);
+        log.debug(uriComponentsBuilder.toUriString());
         return userData;
     }
 }
